14f21bf4b4cd37372de3f745dffda38cdb0e92cf
Anarkrypto <kaiquenunes23@gmail.com>
2019-10-27 08:31:05 -0300

    API v0.0.1

--- api.py
+++ api.py
@@ -1,26 +1,76 @@
 from flask import Flask, jsonify, request
-from functions import block_create, check_balance, check_block, broadcast, worker_account, minimum_fee
+from functions import block_create, balance, frontier, broadcast, worker_account, min_fee, max_multiplier, use_active_difficulty, get_difficulty, solve_work, service_port, get_my_ip, worker_private_key, worker_representative, general_account, sign_new_account_code, sign_new_ip_code, pending_filter, receive, check_history, register, encode_ip, check_key_pair, node
 from nanolib import Block
-
 import requests
 import json
+import waitress
 
 app = Flask(__name__)
 
+#Check inputs
+if check_key_pair(worker_account, worker_private_key) is False:
+    print ("Invalid key pair")
+    quit()
+#...
+print ("Configurations okay")
+
+#Check if node is online
+try:
+    r = requests.post(node, json={"action": "version"})
+except:
+    print ("Node " + node + " Offline! Exiting")
+    quit()
+else:
+    if 'node_vendor' in r.json():
+        print ("Node (" + r.json()["node_vendor"] + ") is online on " + node)
+    else:
+        print ("Node " + node + " error! Exiting")
+        quit()
+
+#Registration Process
+print ("Checking register")
+history = check_history(worker_account, general_account)
+if history is not None:
+    if int(history["amount"]) == sign_new_account_code:
+        print ("Found your worker account registration: " + worker_account)
+        myIP = get_my_ip() #check IP register
+        ip_account = encode_ip(myIP)
+        history = check_history(worker_account, ip_account)
+        if history is not None:
+            print ("Found your actuall IP address registration: " + myIP)
+            if int(history["amount"]) != sign_new_ip_code:
+                print ("Incorrect amount in your IP address register")
+                register (worker_account, worker_representative, frontier(worker_account), ip_account, sign_new_ip_code, 1.0)
+        else:
+            print ("Not found your actuall IP address registration: " + myIP)
+            register (worker_account, worker_representative, frontier(worker_account), ip_account, sign_new_ip_code, 1.0)
+    else:
+        print ("Incorrect amount in register")
+        register (worker_account, worker_representative, frontier(worker_account), general_account, sign_new_account_code, 1.0)
+else:
+    print ("Not found your worker registration: " + worker_account)
+    register (worker_account, worker_representative, frontier(worker_account), general_account, sign_new_account_code, 1.0)
+
+
+#convert to raws
+fee = int(min_fee * 1000000000000000000000000000000)
 
 @app.route('/open_request', methods=['GET', 'POST'])
 def opening_request():
-    return {
-	"reward_address": worker_account,
- 	"minimum_fee": minimum_fee,
-    }
+    print ("Open Request")
+    header = {"version": "0.0.1", "reward_account": worker_account, "fee": fee, "max_multiplier": max_multiplier}
+    if use_active_difficulty == True:
+        header["min_multiplier"] = get_difficulty()
+    else:
+        header["min_multiplier"] = 1.0
+    return header
 
 
-@app.route('/request_pow', methods=['GET', 'POST'])
-def request_pow():
-
+@app.route('/request_work', methods=['GET', 'POST'])
+def request_work():
     #request transactions
     data = request.get_json()
+    global fee
 
     #check if user and worker transaction are present
     if "user_transaction" in data:
@@ -29,7 +79,7 @@ def request_pow():
         print ("User transaction missing")
         return {"Error": "User transaction missing"}
 
-    if "user_transaction" in data:
+    if "worker_transaction" in data:
         worker = data.get("worker_transaction")
     else:
         print ("Worker transaction missing")
@@ -37,11 +87,11 @@ def request_pow():
 
 
     #Read transaction and check if is valid
-    user_block = block_create(user.get('block_type'), user.get('account'), user.get('representative'), user.get('previous'), user.get("link_as_account"), user.get("signature"), int(user.get('balance')))
+    user_block = block_create(user.get('block_type'), user.get('account').replace("xrb_", "nano_"), user.get('representative'), user.get('previous'), user.get("link_as_account"), int(user.get('balance')), user.get("signature"))
     if user_block == "invalid":
         print ("Invalid user transaction")
         return '{"Error": "Invalid user transaction"}'
-    worker_block = block_create(worker.get('block_type'), worker.get('account'), worker.get('representative'), worker.get('previous'), worker.get("link_as_account"), worker.get("signature"), int(worker.get('balance')))
+    worker_block = block_create(worker.get('block_type'), worker.get('account').replace("xrb_", "nano_"), worker.get('representative'), worker.get('previous'), worker.get("link_as_account"), int(worker.get('balance')), worker.get("signature"))
     if worker_block == "invalid":
         print ("Invalid worker transaction")
         return {"Error": "Invalid worker transaction"}
@@ -56,55 +106,71 @@ def request_pow():
         print("Worker account is incorrect")
         return {"Error": "Worker account is incorrect"}
 
-
     #Check if previous block in worker hash is correct
     if worker_block.previous != user_block.block_hash:
         print ("Incorrect previous block in worker block")
         return {"Error": "Incorrect previous block in worker block" }
 
-    #Check if fee is right
-    fee = user_block.balance - worker_block.balance
-    if fee  < minimum_fee:
-        print ( "Fee " + str(fee) + " is less than minimum " + str(minimum_fee))
-        return {"Error": "Fee " + str(fee) + " is less than minimum " + str(minimum_fee)}
+    #Recalculate the Fee with active_difficulty with 10% tolerance
+    if use_active_difficulty == True:
+        multiplier = get_difficulty()
+        if (multiplier > max_multiplier):
+            return {"Error": "Maximum difficulty exceded"}
+        else:
+            print ("Using active difficulty: " + str(multiplier))
+        if multiplier * 0.9 > 1.0:
+            fee *= (multiplier * 0.9) #multiplier fee with tolerance
+        else:
+            fee *= 1.0
+    else:
+        multiplier = 1.0
 
-    #Check if user block already exists
-    if check_block(user_block.block_hash) == "exists":
-        print ("User block already exists")
-        return {"Error": "User block already exists"}
+    #Check if fee is right
+    user_fee = user_block.balance - worker_block.balance
+    if user_fee < fee:
+        print ( "Fee " + str(user_fee) + " is less than minimum " + str(fee))
+        return {"Error": "Fee is less than minimum"}
+
+    #Check previous block
+    if frontier(user_block.account) == user_block.previous:
+        print ("Block is valid: " + user_block.block_hash)
+    else:
+        print ("Gap previous block")
+        return {"Error": "Gap previous block"}
 
     #Check if account source has sufficient funds for both transactions
-    if check_balance(user_block.account, int(worker_block.balance)) is False:
+    if balance(user_block.account) < int(worker_block.balance):
         print ("Insufficient funds")
         return {"Error": "Insuficient funds"}
     else:
         print ("Account has sufficient funds")
 
-
-    #If all is right, PoW both transactions
-    user_block.solve_work()
+    #If all is right, check active_difficulty and PoW both transactions
+    print ("Solving Works...")
+    user_block.work = solve_work(user_block.previous, multiplier)
     print ("User transaction: work done")
-    worker_block.solve_work()
+    worker_block.work = solve_work(worker_block.previous, multiplier)
     print ("Worker transaction: work done")
-
 
     #Broadcast
     r_user = broadcast(user_block.json())
     r_worker = broadcast(worker_block.json())
 
-
     #response
     if 'hash' in r_user:
-        print ("User transaction successful! Block:" + r_user.get("hash") + "\n")
+        print ("User transaction successful! Block:" + r_user.get("hash"))
         response = {"Successful": r_user["hash"]}
 
         if 'hash' in r_worker:
-            print ("Worker transaction successful! Block:" + r_user["hash"] + "\n")
+            print ("Worker transaction successful! Block:" + r_worker["hash"])
+            print ("Your reward has arrived!")
         else:
-            print ("Worker transaction fail. Details: " + json.dumps(r_worker) + "\n")
-
+            print ("Worker transaction fail. Details: " + json.dumps(r_worker))
     else:
         print ("User transaction fail. Details: " + json.dumps(r_user) + "\n")
         response = {"Error": "User transaction fail. Details: " + json.dumps(r_user)}
 
+    print ("\n")
     return response
+
+waitress.serve(app, host='0.0.0.0', port=service_port)
--- functions.py
+++ functions.py
@@ -1,17 +1,36 @@
-from nanolib import Block
+from nanolib import Block, get_account_id
 import json
 import requests
 
-#Import config
-with open('config.json') as config_file:
-    data = json.load(config_file)
+#Convert IP address to valid url format
+def toURL(address):
+    if not address.startswith("http"):
+        return "http://" + address
+    else:
+        return address
 
-worker_account = data['reward_address']
-minimum_fee = data['minimum_fee']
-node = data['node_url']
+#Import config
+with open('worker_config.json') as worker_config:
+    data = json.load(worker_config)
+worker_account = data['reward_account'].replace("xrb_", "nano_")
+worker_private_key = data['private_key']
+worker_representative = data['representative'].replace("xrb_", "nano_")
+min_fee = data['fee']
+node = toURL(data['node'])
+worker_node = toURL(data['worker'])
+use_active_difficulty = data['use_active_difficulty']
+max_multiplier = data['max_multiplier']
+service_port = data['port']
+
+with open('register_config.json') as register_config:
+    data_register = json.load(register_config)
+general_account = data_register['general_account'].replace("xrb_", "nano_")
+sign_new_ip_code = int(data_register['sign_new_ip_code'])
+sign_new_account_code = int(data_register['sign_new_account_code'])
+get_ip = toURL(data_register['get_ip'])
 
 #Read transaction Block
-def block_create(block_type, account, representative, previous, link_as_account, signature, balance):
+def block_create(block_type, account, representative, previous, link_as_account, balance, signature):
     try:
         block = Block(
             block_type=block_type,
@@ -19,43 +38,151 @@ def block_create(block_type, account, representative, previous, link_as_account,
             representative=representative,
             previous=previous,
             link_as_account=link_as_account,
-            signature=signature,
-            balance=balance
+            balance=balance,
         )
     except:
         return "invalid"
     else:
+        if signature is not None:
+            try:
+                block.signature = signature
+            except:
+                return "invalid"
         return block
 
-
 #check if the account has the balance
-def check_balance(address, balance):
-    data = {
-        "action": "account_balance",
-        "account": address
-    }
-    resp = json.loads(requests.post(node, json=data).text)
-    if int(resp['balance']) >= int(balance):
-        return True
-    return False
+def balance(account):
+    resp = requests.post(node, json={"action": "account_balance", "account": account})
+    return int(resp.json()['balance'])
 
+#account history
+def check_history(account, destination):
+    request = requests.post(node, json={"action": "account_history", "account": account, "count": -1})
+    history = request.json()["history"]
+    for b in history:
+        if destination == b["account"].replace("xrb_", "nano_"):
+            return b
+    return (None)
 
 #check if block already exists
-def check_block(hash):
-    data = {
-        "action": "block_info",
-        "json_block": "true",
-        "hash": hash
-    }
-    resp = json.loads(requests.post(node, json=data).text)
-    if 'error' in resp:
-        return "valid"
-    else:
-        return "exists"
+def frontier(account):
+     request = requests.post(node, json={"action": "accounts_frontiers", "accounts": [account]})
+     if account in request.json()["frontiers"]:
+        return request.json()["frontiers"][account]
+     else:
+        request = requests.post(node, json={ "action": "account_key", "account" : account })
+        return request.json()["key"]
 
+#active difficulty network
+def get_difficulty():
+     request = requests.post(node, json={"action": "active_difficulty", "include_trend": "true"})
+     return float(request.json()["multiplier"])
 
+#solve work
+def solve_work (hash, multiplier):
+     request = requests.post(worker_node, json={"action": "work_generate", "hash": hash, "multiplier": multiplier })
+     return request.json()["work"]
 
 #broadcast transaction
 def broadcast(transaction):
     request = requests.post(node, json={"action": "process", "json_block": "true", "block": json.loads(transaction)})
-    return json.loads(request.text)
+    return request.json()
+
+def pending_filter (account, threshold):
+    request = requests.post(node, json={"action": "pending", "account": account, "count": "-1", "threshold": "1"})
+    blocks = request.json()["blocks"]
+    if blocks == "":
+        return None
+    else:
+        if len(blocks) == 1:
+            return blocks
+        else:
+            for key in blocks:
+                    blocks[key] = int(blocks[key])
+            sorted_blocks = sorted(blocks.items(), key=lambda kv: kv[1], reverse=True)
+            for key in sorted_blocks:
+                receive_blocks[key] = sorted_blocks[key]
+                acumulated += sorted_blocks[key]
+                if acumulated >= threshold:
+                    return receive_blocks
+            return None
+
+def receive(account, private_key, representative, amount, link, difficulty):
+     request = requests.post(node, json={"action": "accounts_frontiers", "accounts": [account]})
+     if account in request.json()["frontiers"]:
+        previous = request.json()["frontiers"][account]
+     else:
+         previous = "0000000000000000000000000000000000000000000000000000000000000000"
+     block = Block(
+        block_type="state",
+        account=account,
+        representative=representative,
+        previous=previous,
+        balance=balance(worker_account) + amount,
+        link=link
+     )
+     block.work = solve_work(frontier(account), difficulty)
+     block.sign(private_key)
+     print (block.json())
+     r = broadcast(block.json())
+     return r
+
+
+def send (account, representative, previous, link_as_account, amount, difficulty):
+    block = block_create("state", account, representative, previous, link_as_account, balance(worker_account) - amount, None)
+    block.sign(worker_private_key)
+    block.work = solve_work(block.previous, difficulty)
+    r = broadcast(block.json())
+    return r
+
+def check_key_pair(account, private_key):
+    if account == get_account_id(private_key=private_key, prefix="nano_"):
+        return True
+    else:
+        return False
+
+def get_my_ip():
+    myIP = requests.get(get_ip)
+    return myIP.text
+
+#encode IPv4 or IPv6 into Nano Account format
+def encode_ip (ip):
+    ip_as_bytes = bytes(map(int, ip.split('.'))) #convert to bytes
+    ip_as_bytes += (32 - len(ip_as_bytes)) * b'\0' #padding
+    ip_account =  get_account_id(public_key=ip_as_bytes.hex(), prefix="nano_") ##convert to nano account format
+    return ip_account
+
+
+def register (account, representative, previous, register_account, code, multiplier):
+    if code == sign_new_account_code:
+        what = "your worker account"
+    if code == sign_new_ip_code:
+        what = "your IP address"
+
+    if balance(account) >= code:
+        print ("You have sufficient funds to register " + what)
+        print ("Registering " + what + " now")
+        r = send(account, representative, previous, register_account, code, multiplier)
+        if 'hash' in r:
+            print ("Successfully registred " + what + " Block:" + r["hash"])
+        else:
+            print ("Register transaction fail. Details: " + json.dumps(r))
+    else:
+        print ("No funds detected, checking if exists unpocketed transactions...")
+        pending = pending_filter(account, sign_new_account_code + sign_new_ip_code )
+        if pending == None:
+            print ("You have not sufficient funds to register " + what + "! Please send at least " + str((sign_new_account_code + sign_new_ip_code)) + " raws to your worker account")
+        else:
+            for block in pending:
+                print ("Receiving pending block: " + str(pending[block]) + " raws")
+                r = receive(account, worker_private_key, frontier(account), representative, int(pending[block]), block, multiplier)
+                if 'hash' in r:
+                    print ("Transaction received!" + " Block:" + r["hash"])
+                else:
+                    print ("Transaction receiving fail. Details: " + json.dumps(r))
+            print ("Ok, registering " + what + " now")
+            r = send(account, representative, frontier(account), register_account, code, multiplier)
+            if 'hash' in r:
+                print ("Successfully registred " + what + " Block:" + r["hash"])
+            else:
+                print ("Register transaction fail. Details: " + json.dumps(r))
